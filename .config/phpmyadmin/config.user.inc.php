2885b3b763e728e3e9cbfb78c43de2a872abf0b5
Rasmus Frederiksen <rasmus@homologation.dk>
2020-09-06 21:48:25 +0200

    Added albeka + made pma-network the network name (instead of the default <SERVICE>_pma-network that docker makes it

--- .config/phpmyadmin/config.user.inc.php
+++ .config/phpmyadmin/config.user.inc.php
@@ -1,7 +1,7 @@
 <?php
 $i = 1;
 $cfg['Servers'][$i]['auth_type'] = 'cookie';
-$cfg['Servers'][$i]['host'] = 'mysql8.0.17';
+$cfg['Servers'][$i]['host'] = 'albeka_db';
 $cfg['Servers'][$i]['compress'] = false;
 $cfg['Servers'][$i]['AllowNoPassword'] = false;
 $cfg['Servers'][$i]['port'] = 3306;
